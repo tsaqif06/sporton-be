6395f7d2e9451311710c26a6221ed1e15495738c
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-02-01 17:46:53 +0700

    fix: refactor database connection for vercel serverless compatibility

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app";
 
+dotenv.config();
 
 const PORT = process.env.PORT || "5001";
-const MONGO_URI =
-  process.env.MONGO_URI || "no-mongo-uri";
+const MONGO_URI = process.env.MONGO_URI || "";
 
-mongoose.connect(MONGO_URI).then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}).catch((error) => {
-    console.error("Error connecting to MongoDB", error);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Error connecting to MongoDB", error));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
