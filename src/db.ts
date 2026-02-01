6056e5488a4bf092cc075c4c3fd370aee0724471
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-02-01 18:53:35 +0700

    refactor: adapt express app for vercel serverless

---
+++ src/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI as string;
+
+if (!MONGO_URI) {
+  throw new Error("❌ MONGO_URI not set");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
