aeb4bbab0075f5c12651cc4d69d57657f24cbee7
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-02-01 13:53:39 +0700

    feat: register all core routes and static folder in app setup

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,28 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
-import { authenticate } from "./middlewares/auth.middleware";
+import categoryRoutes from "./routes/category.routes";
+import productRoutes from "./routes/product.routes";
+import bankRoutes from "./routes/bank.routes";
+import transactionRoutes from "./routes/transaction.routes";
+import path from "path";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" })); // limit default = 100kb
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/auth", authRoutes);
 
+app.use("/api/banks", bankRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/transactions", transactionRoutes);
+
 app.get("/", (req, res) => {
   res.send("Sporton Backend API is running...");
-});
-
-app.get("/test-middleware", authenticate, (req, res) => {
-  res.send("Endpoint ini sekarang tidak bisa diakses public");
 });
 
 export default app;
