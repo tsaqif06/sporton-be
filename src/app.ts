56126e346a67299227de380b3df6c8bd56ee2103
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-02-02 04:54:50 +0700

    revert: rollback server configs; migrating from Vercel to Railway

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,21 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-
-import { connectDB } from "./db";
-
 import authRoutes from "./routes/auth.routes";
 import categoryRoutes from "./routes/category.routes";
 import productRoutes from "./routes/product.routes";
 import bankRoutes from "./routes/bank.routes";
 import transactionRoutes from "./routes/transaction.routes";
+import path from "path";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "10mb" })); // limit default = 100kb
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    console.error("DB error:", err);
-    res.status(500).json({ message: "Database connection failed" });
-  }
-});
-
 app.use("/api/auth", authRoutes);
+
 app.use("/api/banks", bankRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
